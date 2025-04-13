13f04e7f2f9d0f43958c3ab24a9751deb71a5252
AdrianOjeda <adrian.16300022@gmail.com>
2025-04-12 22:52:29 -0600

    Hands on 3 completed

---
+++ Hands-on_3/helloworld.sh
@@ -0,0 +1,17 @@
+
+# Imprimir en pantalla “Hello World”
+echo "Hello World"
+
+# Listar el contenido del directorio actual
+echo "Contenido del directorio actual:"
+ls
+
+# Crear un directorio llamado Test
+mkdir -p Test
+
+# Cambiar al directorio Test
+cd Test
+
+# Listar el contenido del directorio Test
+echo "Contenido del directorio Test:"
+ls
